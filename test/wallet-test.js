8ed4b0a17d34c2cb48cddb54fb649e131ffa26a8
TsengMJ <tsngmj@gmail.com>
2022-05-20 12:33:07 +0800

    First version

---
+++ test/wallet-test.js
@@ -0,0 +1,27 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Wallet Contract", function () {
+  describe("Deployment", function () {});
+
+  describe("Ehter", function () {});
+
+  describe("ERC20", function () {});
+
+  describe("ERC721", function () {});
+
+  // it("Should return the new greeting once it's changed", async function () {
+  //   const Greeter = await ethers.getContractFactory("Greeter");
+  //   const greeter = await Greeter.deploy("Hello, world!");
+  //   await greeter.deployed();
+
+  //   expect(await greeter.greet()).to.equal("Hello, world!");
+
+  //   const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+  //   // wait until the transaction is mined
+  //   await setGreetingTx.wait();
+
+  //   expect(await greeter.greet()).to.equal("Hola, mundo!");
+  // });
+});
