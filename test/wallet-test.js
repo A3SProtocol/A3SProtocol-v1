9623bcea83c079fc8338205daac0e8a668b02286
TsengMJ <tsngmj@gmail.com>
2022-05-23 01:21:36 +0800

    install @openzeppelin/contract-upgradable

--- test/wallet-test.js
+++ test/wallet-test.js
@@ -1,14 +1,44 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { ethers, waffle, web3 } = require("hardhat");
 
-describe("Wallet Contract", function () {
-  describe("Deployment", function () {});
+describe("Wallet Contract", () => {
+  let Wallet;
+  let wallet;
+  let provider;
+  let owner;
+  let user1;
+  let user2;
+  let users;
 
-  describe("Ehter", function () {});
+  beforeEach(async () => {
+    provider = waffle.provider;
+    [owner, user1, user2, ...users] = await ethers.getSigners();
+    Wallet = await ethers.getContractFactory("Wallet");
+    wallet = await Wallet.deploy(user1.address);
+  });
 
-  describe("ERC20", function () {});
+  it("Deployment: Should Deploy Wallet Contract", async () => {
+    expect(wallet.address).to.have.length.above(0);
+    expect(await wallet.owner()).to.equal(owner.address);
+  });
 
-  describe("ERC721", function () {});
+  it("Ehter: Can receive ether", async () => {
+    await owner.sendTransaction({
+      to: wallet.address,
+      value: ethers.utils.parseEther("1.0"), // Sends exactly 1.0 ether
+    });
+
+    console.log(await web3.eth.getBalance(wallet.address));
+    console.log(await web3.utils.toWei("1", "ether"));
+
+    expect(await provider.getBalance(wallet.address)).to.equal(
+      await web3.utils.toWei("1", "ether")
+    );
+  });
+
+  it("ERC20", () => {});
+
+  it("ERC721", () => {});
 
   // it("Should return the new greeting once it's changed", async function () {
   //   const Greeter = await ethers.getContractFactory("Greeter");
