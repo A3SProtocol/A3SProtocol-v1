be0e6682c561c21f4438f1fc0b8ab71a64bb9bbc
TsengMJ <tsngmj@gmail.com>
2022-07-21 16:59:33 +0800

    test A3SWalletFactory Update

--- hardhat.config.js
+++ hardhat.config.js
@@ -26,7 +26,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
   networks: {
     hardhat: {
       allowUnlimitedContractSize: true,
